7d1f56bef500c66410e39adb9b536aa04b56471f
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-15 07:21:38 +0500

    Add UserResource and RegisterRequest for user registration

---
+++ app/Http/Resource/UserResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resource;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    final function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'username' => $this->username,
+            'profile_photo' => $this->profile_photo,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
