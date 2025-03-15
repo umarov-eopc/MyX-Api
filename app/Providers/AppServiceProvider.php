299395b377ea7587e9e236accc1f6b0d603107f8
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-15 09:28:35 +0500

    Refactor routing in web.php to use API middleware for registration and user retrieval

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,18 +6,12 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    final function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    final function boot(): void
     {
         //
     }
