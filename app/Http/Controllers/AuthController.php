4836fdceadba5da196b694d5cc16d7485526483c
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-15 08:10:45 +0500

    Enhance AuthController to return UserResource and access token on registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resource\UserResource;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Intervention\Image\Image;
 
 class AuthController extends Controller
 {
-    final function register(RegisterRequest $request)
+    final function register(RegisterRequest $request): JsonResponse
     {
         $data = $request->validated();
         if ($request->hasFile('profile_photo')) {
@@ -26,6 +29,17 @@ class AuthController extends Controller
 
         $user = User::create($data);
 
-        $token = $user->createToken
+        $token = $user->createToken('auth_token')->accessToken;
+
+        return response()->json([
+            'user' => new UserResource($user),
+            'token' => $token,
+            'message' => 'User registrated successfully'
+        ], 201);
+    }
+
+    final function user(Request $request): UserResource
+    {
+        return new UserResource($request->user());
     }
 }
