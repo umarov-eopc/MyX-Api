cf963fdce15b9f09d128c2583806d7d6c32aa034
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-15 20:42:52 +0500

    Add routes for PostController; implement index, store, and show methods

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,13 @@ Route::middleware(['api'])->prefix('api')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->withoutMiddleware([VerifyCsrfToken::class]);
     Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware([VerifyCsrfToken::class]);
     Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
+
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('/user', [AuthController::class, 'user']);
+
+        Route::get('/posts', [PostController::class, 'index']);
+        Route::post('/posts', [PostController::class, 'store']);
+        Route::get('/posts/{post}', [PostController::class, 'show']);
+    });
 });
