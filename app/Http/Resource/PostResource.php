27be7d750cd888e79a737564e8aa3b3f5e62eeca
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-15 14:59:04 +0500

    Add Post model and resource; establish relationship with User model

---
+++ app/Http/Resource/PostResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resource;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    final function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'content' => $this->content,
+            'photo' => $this->photo ? url('storage/' . $this->photo) : null,
+            'user' => new UserResource($this->user),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
