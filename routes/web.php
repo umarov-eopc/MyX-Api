d5e75280f73a6bb448b6015330f831bd24a0df9c
Umarov Ismoiljon <hs.umarov21@gmail.com>
2025-03-16 21:55:07 +0500

    Refactor Sanctum and session configuration; update guards and middleware settings

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['api'])->prefix('api')->group(function () {
-    Route::post('/register', [AuthController::class, 'register'])->withoutMiddleware([VerifyCsrfToken::class]);
-    Route::post('/login', [AuthController::class, 'login'])->withoutMiddleware([VerifyCsrfToken::class]);
-    Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-
-    Route::middleware('auth:api')->group(function () {
-        Route::post('/user', [AuthController::class, 'user']);
-
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', [AuthController::class, 'user']);
         Route::get('/posts', [PostController::class, 'index']);
         Route::post('/posts', [PostController::class, 'store']);
         Route::get('/posts/{post}', [PostController::class, 'show']);
